13e5ca6ce37fc9fb999311a3bc3d2e2dd5539ab8
Elisacv8 <140852045+Elisacv8@users.noreply.github.com>
2025-08-14 14:29:38 -0600

    Add files via upload

---
+++ Formulario_tarea.jsx
@@ -0,0 +1,21 @@
+//Usar useState, eventos (on change, onclick)
+//Mostrar listas (.map)
+//Eliminar elementos 
+//Contar tareas 
+function Formulario_tarea (){
+<div>
+    <Header>
+        background <color> 
+        set background(color(000000))
+        </color>
+    </Header>
+</div>
+
+addEventListener <FileList>
+onclick <useState>
+<Mostrar_Listas className="map">
+
+</Mostrar_Listas>
+
+Mostrar_listas <ClipboardEvent(touchList,"Mini_Lista_Tareas")
+}
---
+++ Lista_Tareas.jsx
@@ -0,0 +1,10 @@
+import logo from './Icono_Lista';
+
+function Lista_Tareas(){
+
+    <header>
+    <title>
+       title("Mini_Lista_Tareas") 
+    </title>
+</header>
+}
